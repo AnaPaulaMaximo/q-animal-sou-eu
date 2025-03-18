4ed615cee181cd3a35c2766367696baa8e552488
AnaPaulaMaximo <ana.maximo.senaisp@gmail.com>
2025-03-18 10:38:25 -0300

    primeiro commit

--- app.py
+++ app.py
@@ -1,95 +1,34 @@
-from flask import Flask, request, render_template
+from flask import Flask, render_template, request
 import requests
+import random
 
 app = Flask(__name__)
 
+API_ENDPOINT = 'https://wizard-world-api.herokuapp.com/Elixirs'
+
 @app.route('/', methods=['GET', 'POST'])
-def index():
-    if request.method == 'GET':
-        return render_template('index.html')
+def render_potion():
+    nome_pocao = None
+    efeito_pocao = None
+    erro = None
 
-    escolha = request.form.get('escolha', None)
+    if request.method == 'POST':
+        try:
+            response = requests.get(API_ENDPOINT) 
+            if response.status_code == 200:
+                dados = response.json()
+                if dados: 
+                    pocao_aleatoria = random.choice(dados)
+                    nome_pocao = pocao_aleatoria.get('name', 'Poção Desconhecida')
+                    efeito_pocao = pocao_aleatoria.get('effect', 'Efeito desconhecido')
+                else:
+                    erro = "Nenhuma poção encontrada na API!"
+            else:
+                erro = f"Erro na API: {response.status_code}"
+        except requests.exceptions.RequestException as e:
+            erro = f"Erro ao conectar com a API"
 
-    if not escolha:
-        return render_template('index.html', erro="Você precisa fazer uma escolha!")
-
-    if escolha == 'cat':
-        return render_cat()
-    elif escolha == 'fox':
-        return render_fox()
-    elif escolha == 'dog':
-        return render_dog()
-    else:
-        return render_duck()
-
-
-@app.route('/cat', methods=['GET', 'POST'])
-def render_cat():
-    API_ENDPOINT = 'https://api.thecatapi.com/v1/images/search'
-    if request.method == 'GET':
-        return render_template('cat.html')
-
-    response = requests.get(API_ENDPOINT)
-
-    if response.status_code == 200:
-        dados = response.json()
-        url_imagem = dados[0]['url']
-        return render_template('cat.html', url_img=url_imagem)
-    else:
-        print(response.status_code)
-        return render_template('cat.html', erro="Erro no sistema! O gato sumiu!")
-
-
-@app.route('/fox', methods=['GET', 'POST'])
-def render_fox():
-    API_ENDPOINT = 'https://randomfox.ca/floof/'
-    if request.method == 'GET':
-        return render_template('fox.html')
-
-    response = requests.get(API_ENDPOINT)
-
-    if response.status_code == 200:
-        dados = response.json()
-        url_imagem = dados['image']
-        return render_template('fox.html', url_img=url_imagem)
-    else:
-        print(response.status_code)
-        return render_template('fox.html', erro="Erro no sistema! A raposa sumiu!")
-
-
-@app.route('/dog', methods=['GET', 'POST'])
-def render_dog():
-    API_ENDPOINT = 'https://random.dog/woof.json'
-    if request.method == 'GET':
-        return render_template('dog.html')
-
-    response = requests.get(API_ENDPOINT)
-
-    if response.status_code == 200:
-        dados = response.json()
-        url_imagem = dados['url']
-        return render_template('dog.html', url_img=url_imagem)
-    else:
-        print(response.status_code)
-        return render_template('dog.html', erro="Erro no sistema! O cachorro sumiu!")
-
-
-@app.route('/duck', methods=['GET', 'POST'])
-def render_duck():
-    API_ENDPOINT = 'https://random-d.uk/api/random'
-    if request.method == 'GET':
-        return render_template('duck.html')
-
-    response = requests.get(API_ENDPOINT)
-
-    if response.status_code == 200:
-        dados = response.json()
-        url_imagem = dados['url']
-        return render_template('duck.html', url_img=url_imagem)
-    else:
-        print(response.status_code)
-        return render_template('duck.html', erro="Erro no sistema! O pato sumiu!")
-
+    return render_template('index.html', nome=nome_pocao, efeito=efeito_pocao, erro=erro)
 
 if __name__ == '__main__':
     app.run(debug=True)
